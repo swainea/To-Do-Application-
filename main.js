ab1cda964e24a8a2c16f1b7e1feda0a17459c3b9
Liz Swain <swainea@gmail.com>
2016-03-25 17:17:49 -0400

    working on the complete button functions

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ function toDoApp () {
 
 //creates a new item when user hits enter
 
+  var itemList = [];
+
   $('form').submit(function newToDo(event){
     event.preventDefault();
 
@@ -16,26 +18,51 @@ function toDoApp () {
   });
 
   $('.items').on('click', 'p', function editListItem(event){
-    $(this).parent('article').addClass('editing');
-
+    $(this).closest('article').addClass('editing');
   });
-
 
   $('.items').on('keyup', '.edit-todo', function enterNewListIten (event){
     if(event.keyCode === 13){
-      // '.edit-todo'.val()); /// i want to put something here that grabs new input, stores it, and spits it out when enter is pressed.
+      $(this).closest('article').find('p').text( $(this).val());
       $(this).closest('article').removeClass('editing');
-
     }
   });
-// this is applying to all li instead of the targeted on click
   $('.items').on('click', '.check', function completeItemClick(event){
     $(this).closest('article').toggleClass('completed');
   });
-// this is applying to all li instead of the targeted on click
+
   $('.items').on('click', '.delete', function deleteItem (event){
-    $(this).closest('li').remove(); // change this to closest li
+    $(this).closest('li').remove();
   });
+
+  $('.show-all').on('click', function showAll (event){
+    $('article').closest('li').css('display', 'block');
+    $('.show-all').addClass('.active');
+    $('.show-active').removeClass('.active');
+    $('.show-completed').removeClass('.active');
+  });
+//above is working
+  $('.show-active').on('click', function showActive (event){
+    $('article').not('.completed').closest('li').css('display', 'block');
+    $('.completed').closest('li').css('display', 'none');
+    $('.show-all').removeClass('.show-all');
+    $('.show-active').addClass('.active');
+  });
+      // completed will be display then anthing that is not completed (still active, display)
+      // eliminate the show all and show the show active
+//above is working
+  $('.show-completed').on ('click', function showCompleted (event){
+    $('article').not('.active').closest('li').css('display', 'block');
+    // $('.active').closest('li').css('display', 'none'); <--- deletes the button- BAD! 
+    $('.show-all').removeClass('.show-all');
+    $('.show-active').removeClass('.active');
+  });
+  //ABOVE IS NOT WORKING
+  // });
+
+//WHATS LEFT!!!
+  //Need to add in item count //need to add in array // need to work on the display
+    //remember to incremnt and decrement the items for the left button when you are
 
 }
 
